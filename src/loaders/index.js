93822408294766116131595ee14d6f9fc86fe0f0
1w1111789AimonettiJoaquin <Yaimonetti@hotmail.com>
2022-09-18 19:36:59 -0300

    configuración del entorno

---
+++ src/loaders/index.js
@@ -0,0 +1,27 @@
+const ExpressServer = require("./server/expressServer");
+const sequelize = require("./sequelize");
+const config = require("../config");
+const logger = require("./logger");
+/* require("../models/user");
+require("../models/movie");
+require("../models/actor"); */
+
+
+
+module.exports = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    logger.info("Models were synchronized successfully.");
+    const server = new ExpressServer();
+    logger.info("Express Loaded");
+    logger.info("DB loaded and connected");
+    server.start();
+    logger.info(`#######################################
+      Server listening on port: ${config.port}
+      #######################################
+    `);
+  } catch (error) {
+    logger.error("Unable to connect to the database:", error);
+  }
+};
